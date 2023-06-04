97af781798d26b096651015bd311d1bd9dd04286
Ahtesham Akram <ahte02254@gmail.com>
2023-06-04 17:40:53 +0500

    code uploaded

---
+++ class 04/app.js
@@ -0,0 +1,62 @@
+// =========== while loop ==========
+/*
+
+// initialization
+syntax:-
+    while(condition){
+        // body of the loop
+        // increement/decrement
+    }
+*/
+
+// let i = 0
+// while(i<10){
+//     console.log("hello world",i);
+//     i++
+// }
+
+// let i = 0
+// do{
+//     console.log("hello world");
+//     i++
+// }while(i<10)
+
+// ========= start: DATA TYPES IN JAVASCRIPT =======
+/*
+    1. primitive
+        . String
+        . Number
+        . Boolean
+        . undefined
+        . null
+        . Symbol
+
+    2. Referene
+        . Arrays
+        . object
+        . Functions
+*/
+// ========= end: DATA TYPES IN JAVASCRIPT =======
+
+// string       https://www.programiz.com/javascript/library/string
+// let str = '  learning javascript '
+let str = 'Learning javascript'
+// const c = str.length
+// const c = str.trim().length
+// const c = str.split("a")
+// console.log(str[str.length-1]);
+// const c = str.charAt(2)
+// const c = str.indexOf("a")   // return first occurence
+// const c = str.lastIndexOf("a")  // return first occurence
+// const c = str.toUpperCase()
+// const c = str.toLowerCase()
+// const c = str.includes("java1234")
+// const c = str.substring(9)
+let a = "one";
+let b = "two";
+const c = b.concat(a)
+
+
+
+
+console.log(c);
---
+++ class 05/app.js
@@ -0,0 +1,93 @@
+
+// ========= start: DATA TYPES IN JAVASCRIPT =======
+/*
+    1. primitive
+        . String
+        . Number
+        . Boolean
+        . undefined
+        . null
+
+    2. Referene
+        . Arrays
+        . object
+        . Functions
+*/
+
+// // let num = 521;
+// let myvar = undefined 
+// console.log(typeof myvar);
+
+// ============ start: Array ====================
+// const arr = new Array("ali","aslam","javeria","noor bano",true,420)
+let arr = ["ali","aslam","javeria","noor bano",true,420];
+// const arr2 = [1,2,3,4,6,7,8]
+// console.log(arr[1],arr[2],arr[2],arr[4]);
+// addition 
+// arr.push("Pushpa");
+// arr.unshift("Pushpa")
+// arr.splice(3,0,"hassan")          // addition
+// arr.splice(3,0,"hassan")          // add with delete
+
+// deletion
+// arr.pop()
+// // let deletedEl = arr.shift()
+// console.log(deletedEl);
+// const ans = arr.concat(arr2)
+// console.log(ans);
+
+// getting all values of an array via loop
+// for(let i=0 ; i<arr.length ; i++){
+//     // console.log(arr[i]);
+// }
+
+// forEach
+// let ans = arr.forEach(function(element){
+//     // console.log(element);
+// })
+
+// //map
+// let ans;
+// ans = arr2.map(function(element){
+//     return element*2 
+// })
+// console.log(ans);   
+
+// filter 
+// let filteredVal = arr2.filter(function(num){
+//    return num>3
+//    sjdhsjds
+// })
+// console.log(filteredVal);
+// console.log(arr2);
+const arr2 = [1,2,3,4,6,7,8,9]
+// indexOf
+// console.log(arr2.indexOf(4));
+
+// join
+// let ans = arr2.join("$")
+// let ans1 = ans.split("$")
+// console.log(ans1);
+
+//reduce
+// let ans = arr2.reduce(function(prev,curr){
+//     return prev+curr
+//     //      0  + 1
+//     //      1 + 2
+//     //      3 + 3
+// },0)
+// console.log(ans);
+
+//reverse
+// console.log(arr2.reverse());
+
+//slice
+// console.log(arr2.slice(4));
+
+//sort
+let myNewArr = [13,245,56,76,38,28,89,83,84]
+console.log('old',myNewArr);
+console.log('new',myNewArr.sort(function(a,b){
+    return a-b
+}));
+// ============ end: Array ====================
