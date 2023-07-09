cd01b56d3f3cbb5bd7efb8160f720bf7c59fe792
devahtesham <ahte02254@gmail.com>
2023-07-09 16:52:37 +0500

    Math object

---
+++ class14/app.js
@@ -0,0 +1,39 @@
+// ========== Math Object =========
+// 1) PI
+// console.log(Math.PI);
+
+// 2) sqrt
+// console.log(Math.sqrt(4));
+
+// 2) power
+// console.log(Math.pow(2, 3));
+
+// 3) cube root
+// console.log(Math.cbrt(8));
+
+// 3) cube root
+// console.log(Math.round(Math.PI));
+// console.log(Math.round(2.4));
+
+// let num = 2.654897454
+// console.log(+(num.toFixed(2)));
+
+// 4) Random
+// let a = Math.random()
+// console.log(a);
+
+// 5) Floor
+// console.log(Math.floor(6.8));
+
+// 5) ceil
+// console.log(Math.ceil(6.1));
+
+// 6) sin
+// console.log(Math.ceil(Math.sin(90)));
+
+// 6) sin
+// console.log(Math.abs(Math.round(Math.cos(90))));
+
+// 7) log
+// console.log(Math.log10(50));
+
